22732e70342ae7442e2d450462b4e666dfc341ec
denis <denismaximov98@gmail.com>
2021-01-17 09:48:08 +0200

    wip: trying to go past the boot rom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+use std::fs::File;
+use std::io::Read;
+use std::env;
+
+pub struct Registers {
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: u8,
+    pub h: u8,
+    pub l: u8,
+    pub flag: FlagRegister
+}
+
+impl Registers {
+    pub fn new() -> Self {
+        Self {
+            a: 0,
+            b: 0,
+            c: 0,
+            d: 0,
+            e: 0,
+            f: 0,
+            h: 0,
+            l: 0,
+            flag: FlagRegister::new()
+        }
+    }
+}
+
+pub struct FlagRegister {
+    pub zero: bool,
+    pub subtract: bool,
+    pub half_carry: bool,
+    pub carry: bool,
+}
+
+impl FlagRegister {
+    pub fn new() -> Self {
+        Self {
+            zero: false,
+            subtract: false,
+            half_carry: false,
+            carry: false
+        }
+    }
+}
+
+pub struct CPU {
+    registers: Registers,
+    pc: u16,
+    sp: u16
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        Self {
+            registers: Registers::new(),
+            pc: 0x0,
+            sp: 0x00
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let mut f = File::open(filename).unwrap();
+    let mut rom = Vec::new();
+
+    f.read_to_end(&mut rom);
+
+    for byte in rom.iter() {
+        println!("{:#?}", byte);
+    }
 }
